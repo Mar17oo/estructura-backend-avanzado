54deddb9c08ed4dd7a15f68b00a17b502abd9e81
Mar Bautista <mar_piscis@live.com.mx>
2024-02-29 20:02:05 -0600

    primer commit

---
+++ server.js
@@ -0,0 +1,15 @@
+//TODO: craer app express
+
+/**
+ * 1.- Importar express
+ * 2.- Crear una aplicación con Express.js y llamada "my-app"
+ * 3.- Usar app.Listen para abrir puertos
+ */
+
+import express  from "express";
+
+const api = express();
+
+api.listen(8000, () =>{
+    console.log("Escuchando en el puerto 8000");
+});
