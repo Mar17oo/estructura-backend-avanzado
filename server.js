d7bc92c04fb4dd4f6cef4cde6681a83fe6a62a90
Mar Bautista <mar_piscis@live.com.mx>
2024-02-29 21:32:18 -0600

    Se añade DB

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  * 2.- Crear una aplicación con Express.js y llamada "my-app"
  * 3.- Usar app.Listen para abrir puertos
  */
-
+import { connect } from './config.js';
 import express  from "express";
 
 const api = express();
